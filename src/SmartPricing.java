f7d5a406ca832821fc33b29788428fe86b3a19bf
Krishna Sharma <krishnasharma18032005@gmail.com>
2026-03-27 05:52:25 +0530

    Update SmartPricing.java

--- src/SmartPricing.java
+++ src/SmartPricing.java
@@ -2,30 +2,23 @@ public class SmartPricingEngine {
 
     public static double suggestPrice(Product product, double minMargin) {
 
-        double cost = product.getCostPrice();
-        double avgCompetitor = ProfitCalculator.averageCompetitorPrice(product);
+        double cost= product.getCostPrice();
+        double avgCompetitor= ProfitCalculator.averageCompetitorPrice(product);
 
         // Minimum safe price
-        double minPrice = cost + (cost * minMargin / 100);
+        double minPrice= cost+(cost*minMargin/100);
 
-        // 1. Competitive pricing
-        double competitivePrice = avgCompetitor - 2;
-
-        // 2. Demand simulation (simple logic)
-        double demandFactor = Math.random(); // 0 to 1
+        double competitivePrice= avgCompetitor- 2;
+        double demandFactor= Math.random();
 
         double demandAdjustedPrice;
-        if (demandFactor > 0.7) {
-            // High demand → increase price
-            demandAdjustedPrice = competitivePrice + 5;
-        } else if (demandFactor < 0.3) {
-            // Low demand → decrease price
-            demandAdjustedPrice = competitivePrice - 5;
+        if (demandFactor>0.7) {
+            demandAdjustedPrice= competitivePrice+ 5;
+        } else if (demandFactor<0.3) {
+            demandAdjustedPrice= competitivePrice- 5;
         } else {
-            demandAdjustedPrice = competitivePrice;
+            demandAdjustedPrice= competitivePrice;
         }
-
-        // 3. Final price should NEVER go below minimum margin
-        return Math.max(demandAdjustedPrice, minPrice);
+        return Math.max(demandAdjustedPrice,minPrice);
     }
 }
